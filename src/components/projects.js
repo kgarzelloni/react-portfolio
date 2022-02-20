bfb962f37be98f50ab628a7299ec68931744ff90
Karrie Garzelloni <kgarzelloni@yahoo.com>
2022-02-19 21:13:05 -0700

    update to images

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,10 @@
 import React from "react";
+// import SwabSwap from 'img/swabswap';
+// import DailySched from 'img/dailysched';
+// import Ecommerce from 'img/ecommerce';
+// import MarkDown from 'img/markdown';
+// import Weather from 'img/weather';
+// import Marvel from '.src/components/img/marvel';
 
 const Projects = () => {
     return (
@@ -7,7 +13,7 @@ const Projects = () => {
             <div className="row g-0">
                 <div className="col-lg-4 col-sm-6"> 
                     <a href="https://kgarzelloni.github.io/06-homework-weather-dashboard/" title="Weather Dashboard">
-                        <img className="img-fluid" src="./img/weather.png" alt="..." /> 
+                        {/* <img className="img-fluid" src={Weather} alt={"weather dashboard"} />  */}
                         <div className="portfolio-box-caption">
                              <div className="project-category text-grey-50">Code Samples</div> 
                              <div className="project-name text-grey-50">Weather Dashboard</div> 
@@ -16,7 +22,7 @@ const Projects = () => {
                 </div>
                 <div className="col-lg-4 col-sm-6">
                     <a href="https://swab-swap.herokuapp.com/" title="Swab Swap">
-                        <img className="img-fluid" src="./img/swabswap.png" alt="..." />
+                        {/* <img className="img-fluid" src={SwabSwap} alt={"application swab swap logo"} /> */}
                         <div className="portfolio-box-caption">
                              <div className="project-category text-grey-50">Group Project</div> 
                              <div className="project-name text-grey-50">Swab Swap</div> 
@@ -25,7 +31,7 @@ const Projects = () => {
                 </div>
                 <div className="col-lg-4 col-sm-6">
                     <a href="https://github.com/kgarzelloni/13-e-commerce-backend" title="E-commerce backend">
-                        <img className="img-fluid" src="./img/ecommerce.png" alt="..." />
+                        {/* <img className="img-fluid" src={Ecommerce} alt={"ecommerce"} /> */}
                         <div className="portfolio-box-caption">
                              <div className="project-category text-grey-50">Code Sample</div> 
                              <div className="project-name text-grey-50">E-commerce backend</div> 
@@ -34,7 +40,7 @@ const Projects = () => {
                 </div>
                 <div className="col-lg-4 col-sm-6">
                     <a href="https://github.com/kgarzelloni/09-README-Generator">
-                        <img className="img-fluid" src="./img/markdown.png" alt="..." />
+                        {/* <img className="img-fluid" src={MarkDown} alt={"readme generator"} /> */}
                         <div className="portfolio-box-caption">
                              <div className="project-category text-grey-50">Code Sample</div> 
                              <div className="project-name text-grey-50">README generator</div> 
@@ -43,7 +49,7 @@ const Projects = () => {
                 </div>
                 <div className="col-lg-4 col-sm-6">
                     <a href="https://emilyshurtz94.github.io/Group-Project-1/" title="Marvel Fan Page">
-                        <img className="img-fluid" src="./img/marvel.png" alt="..." />
+                        {/* <img className="img-fluid" src={Marvel} alt={"Marvel Fanpage"} /> */}
                         <div className="portfolio-box-caption">
                              <div className="project-category text-grey-50">Group Project</div> 
                              <div className="project-name text-grey-50">Marvel Fan Page</div> 
@@ -52,7 +58,7 @@ const Projects = () => {
                 </div>
                 <div className="col-lg-4 col-sm-6">
                     <a href="https://kgarzelloni.github.io/05-work-day-scheduler/" title="Workday scheduler">
-                        <img className="img-fluid" src="./img/dailysched.png" alt="..." />
+                        {/* <img className="img-fluid" src={DailySched} alt={"a workday planner"} /> */}
                         <div className="portfolio-box-caption p-3">
                            <div className="project-category text-grey-50">Code Sample</div> 
                            <div className="project-name text-grey-50">Work Day scheduler</div> 
